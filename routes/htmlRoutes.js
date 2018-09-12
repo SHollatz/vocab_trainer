cd53ce8d5a5118d2e492b78ef4dc273ae8257552
SHollatz <ztallohs@gmail.com>
2018-09-11 21:22:33 -0700

    added styles.css + html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,59 @@
+//var express = require("express");
+
 var db = require("../models");
 
+//console.log(db);
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+    db.Word.findAll({}).then(function(dbWords) {
+      res.render("home", {
         msg: "Welcome!",
-        examples: dbExamples
+        word: dbWords
+      });
+    });
+  });
+
+  app.get("/words", function(req, res) {
+    db.Word.findOne({ where: { word: req.params.word } }).then(function(dbWords) {
+      res.render("words", {
+        word: dbWords
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/words/:word?", function(req, res) {
+    db.Word.findOne({ where: { word: req.params.word } }).then(function(dbWords) {
+      res.render("words", {
+        word: dbWords
+      });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/words/:translation1?", function(req, res) {
+    db.Word.findOne({ where: { translation1: req.params.translation1 } }).then(
+      function(dbWords) {
+        res.render("words", {
+          word: dbWords
+        });
+      }
+    );
+  });
+
+  app.get("/games", function(req, res) {
+    db.Word.findAll({}).then(function(dbWords) {
+      res.render("games", {
+        word: dbWords
+      });
+    });
+  });
+
+  app.get("/about", function(req, res) {
+    db.Word.findAll({}).then(function(dbWords) {
+      res.render("about", {
+        word: dbWords
       });
     });
   });
