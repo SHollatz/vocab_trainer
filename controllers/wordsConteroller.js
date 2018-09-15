20b8c8e407df72e9797cebd7706dec31f035ea51
SHollatz <ztallohs@gmail.com>
2018-09-14 23:32:16 -0700

    done first draft

--- controllers/wordsConteroller.js
+++
@@ -1,8 +0,0 @@
-// translator
-    // provided languages: English, Spanish, etc.
-
-// note card trainer
-
-// add words
-    // input fields for word, translation, and figure of speech
-    // depending on figure of speech provide options.
