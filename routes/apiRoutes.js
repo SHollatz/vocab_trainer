541ed03a49fd6a5d66a1372f049d0a51094e09e8
SHollatz <ztallohs@gmail.com>
2019-06-19 15:34:45 -0700

    fixed vulnderabilities

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
   app.get("/api/words", function(req, res) {
     db.Word.findAll({}).then(function(dbWords) {
       res.json(dbWords);
     });
   });
 
-  // Create a new example
   app.post("/api/words", function(req, res) {
     db.Word.create(req.body).then(function(dbWords) {
       res.json(dbWords);
